fe0637db22d21bd2e9a5334ff24343af34c6a122
kalibani <kalibani.ka@gmail.com>
2023-06-29 04:20:17 +0700

    create a skeleton

---
+++ src/pages/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import {
+  Nav,
+  About,
+  Contact,
+  Experience,
+  Footer,
+  Header,
+  Portfolio,
+  Testimonials,
+  Services,
+} from "../components";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      background: "#1f1f38",
+      "background-variant": "#2c2c6c",
+      primary: "#4db5ff",
+      "primary-variant": "rgba(77, 181, 255, 0.4)",
+      "custom-light": "rgba(255, 255, 255, 0.6)",
+    },
     extend: {},
   },
   plugins: [],
